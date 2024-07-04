3db5f22c91940092b81c9a6be19c950c733cf617
joaozitos1h1 <129197502+joaozitos1h1@users.noreply.github.com>
2024-07-04 09:52:15 -0300

    prova

--- provaPratica/src/Components/Header.jsx
+++
@@ -1,9 +0,0 @@
-const Header = ({ aluno, turma }) => { 
-    return ( 
-        <header> 
-            <h1>{aluno}</h1>
-            <h2>{turma}</h2> 
-        </header> 
-    ) 
-}    
-export default Header;
